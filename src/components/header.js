f931f57425087a16d88bdf21136223a69820feef
Tanguy Andreani <tanguy.andreani@tuta.io>
2022-01-07 18:37:49 +0100

    link to my blog

--- src/components/header.js
+++ src/components/header.js
@@ -13,7 +13,7 @@ export default class Header extends React.Component {
                 <h1 class="text-5xl font-bold font-title">Tanguy Andreani <span class="text-gray-300 font-title">{'{'}</span></h1>
                 <hr class="mb-8 mt-4 border-2 w-32 animate-scalex origin-left"></hr>
                 <div class="text-xl sm:text-2xl text-left">
-                    <MenuLink link="/" text="À propos" color="border-red-400" />
+                    <MenuLink link="https://tanguyandreani.github.io" text="Blog" color="border-red-400" />
                     <MenuLink link="/cv.pdf" text="CV" color="border-yellow-300" />
                     <MenuLink link="mailto:hello@tanguyandreani.me" text="hello@tanguyandreani.me" color="border-blue-300" />
                 </div>
