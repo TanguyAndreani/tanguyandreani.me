d9e4b7c4a9596d2cae8d7f0b5d267c7bfc1e36dc
Tanguy Andreani <hello@tanguyandreani.me>
2022-04-02 15:17:53 +0200

    update about me

--- src/components/about.js
+++ src/components/about.js
@@ -4,13 +4,11 @@ export default class About extends React.Component {
     render() {
         return (
             <div class="mx-auto my-8 font-paragraph font-extralight">
-                <p class="text-lg mt-4">Étudiant en troisième année à Epitech
-                    Nice, je suis à la recherche d'opportunités de stages sur Nice et sur Ajaccio. Je suis aussi disponible pour des missions
-                    freelances. J'ai eu mon BAC S en 2018 et j'ai actuellement 21 ans.</p>
+                <p class="text-lg mt-4">Bonjour, je suis un étudiant Epitech en fin de cursus, à la recherche d'opportunités d'emploi sur toute la France, possiblement en remote. Je suis aussi disponible pour des missions
+                    freelances.</p>
                     
                 <p class="text-lg mt-4">
-                    Ma prochaine période de stage se déroulera à partir
-                    de mi-<u>mars 2022</u> pour 4 à 6 mois à temps plein.
+                    Je recherche avant tous des opportunités de développeur Fullstack bien que tout type d'offre puisse m'intéresser.
                 </p>
             </div>
         )
