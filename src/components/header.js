52eab5813ee0e98378983f924e56224f66878180
Tanguy Andreani <hello@tanguyandreani.me>
2024-07-19 11:51:51 +0200

    link to /blog

--- src/components/header.js
+++ src/components/header.js
@@ -14,7 +14,7 @@ export default class Header extends Component {
                 <h1 class="text-5xl font-title">Tanguy Andreani <span class="text-gray-500 font-title">{'{'}</span></h1>
                 <hr class="mb-8 mt-4 border-2 w-32 animate-scalex origin-left"></hr>
                 <div class="text-xl sm:text-2xl text-left">
-                    <MenuLink link="https://tanguyandreani.github.io" text="Blog" color="border-red-400" />
+                    <MenuLink link="https://tanguyandreani.github.io/blog" text="Blog" color="border-red-400" />
                     <MenuLink link="https://github.com/TanguyAndreani" text="GitHub" color="border-green-400" />
                     <MenuLink link="https://tanguyandreani.github.io/cv_tanguy_andreani.pdf" text="CV" color="border-yellow-300" />
                     <MenuLink link="mailto:hello@tanguyandreani.me" text="hello@tanguyandreani.me" color="border-blue-300" />
